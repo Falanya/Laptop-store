6ffd8972a1d95c55b06b5fd8532839837941e675
= <=>
2023-07-15 23:13:16 +0700

    update

--- admin.php
+++ admin.php
@@ -8,11 +8,14 @@ include './setting/connect.php'
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Admin</title>
+    <link rel="icon" href="./img/logo.jpg">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./Css/styles.css">
+    <link rel="stylesheet" href="./Css/responsive_home.css">
 </head>
 <body>
-    <div class="background-admin">
-        
+    <div class="header_admin">
+                
     </div>
 </body>
 </html>
